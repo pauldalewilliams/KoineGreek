7f9c16bd8a24a88dc906db87db15dc6d5b05bb62
pauldalewilliams <pauldalewilliams@gmail.com>
2017-04-01 17:02:11 -0400

    Add activities

--- app/src/main/java/com/thepantologist/koinegreek/MainActivity.java
+++ app/src/main/java/com/thepantologist/koinegreek/MainActivity.java
@@ -1,7 +1,9 @@
 package com.thepantologist.koinegreek;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openSmallLetters(View view) {
+        Intent i = new Intent(this, SmallLettersActivity.class);
+        startActivity(i);
+    }
+
+    public void openCapitalLetters(View view) {
+        Intent i = new Intent(this, CapitalLettersActivity.class);
+        startActivity(i);
+    }
+
+    public void openDiphthongs(View view) {
+        Intent i = new Intent(this, DiphthongsActivity.class);
+        startActivity(i);
+    }
+
+    public void openVocab1(View view) {
+        Intent i = new Intent(this, Vocab1Activity.class);
+        startActivity(i);
+    }
+
+    public void openVocab2(View view) {
+        Intent i = new Intent(this, Vocab2Activity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/thepantologist/koinegreek/SmallLettersActivity.java
@@ -0,0 +1,13 @@
+package com.thepantologist.koinegreek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmallLettersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_small_letters);
+    }
+}
